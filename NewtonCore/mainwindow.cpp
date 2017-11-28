064e452b47287fd5b1b116e0631cbcaeee971443
Christian <ChrisWcs@users.noreply.github.com>
2017-11-28 16:26:42 -0700

    Add graphicsview, vertical container, add model to be passed in ( needs the correct name of the model class

--- NewtonCore/mainwindow.cpp
+++ NewtonCore/mainwindow.cpp
@@ -1,9 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
+MainWindow::MainWindow(QWidget *parent, Model *model) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    model(model)
 {
     ui->setupUi(this);
 }
--- NewtonCore/mainwindow.h
+++ NewtonCore/mainwindow.h
@@ -12,11 +12,12 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = 0, Model *model);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+    Model *model;
 };
 
 #endif // MAINWINDOW_H
