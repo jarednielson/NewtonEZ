00fec481723f6d97c33e9642fc89919081c025dd
jarednielson <jarednielsonn@gmail.com>
2017-11-30 14:37:05 -0700

    Start model framework

--- NewtonCore/newtonmodel.cpp
+++ NewtonCore/newtonmodel.cpp
@@ -1,4 +1,19 @@
-#include "newtongraphicsview.h"
-#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QObject>
 #include "newtonmodel.h"
+
+NewtonModel::NewtonModel(QObject *parent) : QObject(parent)
+{
+    currenSceneIndex = 0;
+    graphicsScene = new graphicsScene();
+}
+
+NewtonModel::NewtonModel(QString filePath, QObject *parent) : QObject (parent)
+{
+    loadFile(filePath);
+}
+
+NewtonModel::~NewtonModel(){
+    delete graphicsScene;
+}
 
