d93d35b7c3bdb168005e3af67caad78adc7d058c
Nathan Milot <nathan.c.milot@gmail.com>
2017-12-05 13:54:34 -0700

    Fix adding model to the scene

--- NewtonCore/mainwindow.cpp
+++ NewtonCore/mainwindow.cpp
@@ -11,8 +11,8 @@ MainWindow::MainWindow(NewtonModel *model, QWidget *parent) :
     ui(new Ui::MainWindow),
     model(model)
 {
-    //ui->graphicsView->setScene(model->getGraphicsScene());
     ui->setupUi(this);
+    ui->graphicsView->setScene(model->getGraphicsScene());
     replaceAndSetText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
     addInputPair("1");
