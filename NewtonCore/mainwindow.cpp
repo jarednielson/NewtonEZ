4fdbe4a9efb17fa7ef2fbdbda82bf21e85b449c1
Nathan Milot <nathan.c.milot@gmail.com>
2017-12-05 13:41:28 -0700

    Minor changes to use instruction text area update method

--- NewtonCore/mainwindow.cpp
+++ NewtonCore/mainwindow.cpp
@@ -13,7 +13,7 @@ MainWindow::MainWindow(NewtonModel *model, QWidget *parent) :
 {
     //ui->graphicsView->setScene(model->getGraphicsScene());
     ui->setupUi(this);
-    ui->textEdit->setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+    replaceAndSetText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
     addInputPair("1");
     addInputPair("2", 2.0, false);
