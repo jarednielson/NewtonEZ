68a55979bf8bb3fd9f7e987b2aa01d176e58676e
Nathan Milot <nathan.c.milot@gmail.com>
2017-12-04 22:13:01 -0700

    Add method to set input box values and enable them or not

--- NewtonCore/mainwindow.cpp
+++ NewtonCore/mainwindow.cpp
@@ -16,15 +16,15 @@ MainWindow::MainWindow(NewtonModel *model, QWidget *parent) :
     ui->textEdit->setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
     addInputPair("1");
-    addInputPair("2");
-    addInputPair("3");
-    addInputPair("4");
-    addInputPair("5");
-    addInputPair("6");
-    addInputPair("7");
-    addInputPair("8");
-    addInputPair("9");
-    addInputPair("10");
+    addInputPair("2", 2.0, false);
+    addInputPair("3", 3.0, false);
+    addInputPair("4", 4.0, false);
+    addInputPair("5", 5.0, false);
+    addInputPair("6", 6.0, false);
+    addInputPair("7", 7.0, false);
+    addInputPair("8", 8.0, false);
+    addInputPair("9", 9.0, false);
+    addInputPair("10", 10.0, true);
     // Comment out to see inputs, uncomment to clear all inputs
     //clearInputConAndVector();
 
@@ -56,17 +56,27 @@ void MainWindow::onGraphicsViewReleased(int x, int y){
 /// \param labelText
 ///
 void MainWindow::addInputPair(QString labelText){
+    MainWindow::addInputPair(labelText, 0.0, true);
+}
+
+///
+/// \brief MainWindow::addInputPair
+/// Helper method that will create a new input pair ( QLabel and QDoubleSpinBox ) placing it
+/// in its own new QHBoxLayout.
+///
+/// \param labelText
+///
+void MainWindow::addInputPair(QString labelText, double inputBoxValue, bool inputEnabled){
     labels.push_back(new QLabel());
     labels.back()->setText(labelText);
     inputs.push_back(new QDoubleSpinBox());
+    inputs.back()->setValue(inputBoxValue);
+    inputs.back()->setEnabled(inputEnabled);
     layouts.push_back(new QHBoxLayout());
     layouts.back()->addWidget(labels.back());
     layouts.back()->addWidget(inputs.back());
     ui->inputContainer->addLayout(layouts.back());
     ui->inputContainer->activate();
-
-//    ui->listView->
-
 }
 
 ///
