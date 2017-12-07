e3179a5ec55b3ff5a382464e613397018cb28f92
echoream <erikluor@gmail.com>
2017-12-07 12:35:02 -0700

    Set up box2d world, wall, shape

---
+++ NewtonCore/boxscene.h
@@ -0,0 +1,22 @@
+#ifndef BOX2DSCENE_H
+#define BOX2DSCENE_H
+
+#define pixelToMeter 30
+#include "Box2D.h"
+
+class BoxScene : public QObject{
+    Q_OBJECT
+private:
+    b2World* world;
+    b2Body* walls;
+    float32 wallLineOffset;
+
+    void RunScene();
+    void buildWorld();
+    void buildWalls();
+    void buildRect();
+    void buildCirc();
+
+};
+
+#endif // BOX2DSCENE_H
