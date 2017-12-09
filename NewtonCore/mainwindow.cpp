e3b249b2d2d55403c73c33374fed3a77605c6bbd
Nathan Milot <nathan.c.milot@gmail.com>
2017-12-09 14:23:41 -0700

    Fix for dark theme to have white background

--- NewtonCore/mainwindow.cpp
+++ NewtonCore/mainwindow.cpp
@@ -54,6 +54,7 @@ MainWindow::MainWindow(NewtonModel *model, QWidget *parent) :
 //    this->setPalette(darkPalette);
 
     ui->graphicsView->setScene(model->getGraphicsScene());
+    ui->graphicsView->setBackgroundBrush(QBrush(Qt::white));
     //ui->graphicsView->setSceneRect(0.0f, 0.0f, 200, 200);
     ui->graphicsView->setTransform(QTransform::fromScale(1, - 1));
     setProblemText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
