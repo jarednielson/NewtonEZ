facbd2b9e07504c2e0e9921c23f7b5c3fdde9c64
Nathan Milot <nathan.c.milot@gmail.com>
2017-12-09 11:27:20 -0700

    Make main graphics view background white for cross platform visability

--- NewtonCore/mainwindow.cpp
+++ NewtonCore/mainwindow.cpp
@@ -54,7 +54,8 @@ MainWindow::MainWindow(NewtonModel *model, QWidget *parent) :
 //    this->setPalette(darkPalette);
 
     ui->graphicsView->setScene(model->getGraphicsScene());
-    ui->graphicsView->setTransform(QTransform::fromScale(0.1, 0.1));
+    ui->graphicsView->setBackgroundBrush(QBrush(Qt::white));
+    ui->graphicsView->setTransform(QTransform::fromScale(0.5, 0.5));
     setProblemText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
     isSimPlaying = false;
 
